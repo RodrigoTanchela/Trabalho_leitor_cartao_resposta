fef4a08185e7b1f52676d0f6868eb11fd551f6c9
Rodrigo Martinez Tanchela <rmtanchela@gmail.com>
2023-10-23 09:22:05 -0300

    Segundas alterações

---
+++ View/Formulario.py
@@ -0,0 +1,24 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+
+class Formulario:
+    def __init__(self):
+        pass
+    #     self.nome = ""
+    #     self.email = ""
+    #
+    # def preencher_formulario(self, nome, email):
+    #     self.nome = nome
+    #     self.email = email
+    #
+    # def gerar_pdf(self, nome_arquivo):
+    #     doc = SimpleDocTemplate(nome_arquivo, pagesize=letter)
+    #     story = []
+    #
+    #     styles = getSampleStyleSheet()
+    #     story.append(Paragraph(f'Nome: {self.nome}', styles['Normal']))
+    #     story.append(Spacer(1, 12))
+    #     story.append(Paragraph(f'Email: {self.email}', styles['Normal']))
+    #
+    #     doc.build(story)
--- View/TelaLeitorCartao.py
+++ View/TelaLeitorCartao.py
@@ -1,32 +1,34 @@
 import PySimpleGUI as sg
+import sys
+sys.path.append(r'C:\Users\Rodrigo\Documents\TrabalhoPin3\ProjetoPin3\View\Formulario')
 
 class TelaLeitorCartao:
     def __init__(self, controlador):
         self.controlador = controlador
 
         layoutBuscarArquivo = [
-            [sg.Text('Caminho',size=(7,0)),sg.Input(size=(51,8), key='arquivo'), sg.Button('Buscar', key='buscar')]
+            [sg.Text('Caminho', size=(7, 0)), sg.Input(size=(51, 8), key='arquivo'), sg.Button('Buscar', key='buscar')]
         ]
 
         layoutNumeroPerguntas = [
-            [sg.Text('Perguntas:',size=(7)), sg.Input(size=(7,0), pad=((0, 200), (0, 0)), key='numeroperguntas'), 
-            sg.Text('Opções:',size=(7,0),), sg.Input(size=(7,0),  key='numeroopcoes')]
+            [sg.Text('Perguntas:', size=(7)), sg.Input(size=(7, 0), pad=((0, 200), (0, 0)), key='numeroperguntas'),
+             sg.Text('Opções:', size=(7, 0), ), sg.Input(size=(7, 0), key='numeroopcoes')]
         ]
-        
+
         layoutEspacamentoMarcador = [
-            [sg.Text('Largura:',size=(7)), sg.Input(size=(7,0), pad=((0, 200), (0, 0)), key='numeroperguntas'), 
-            sg.Text('Altura:',size=(7,0),), sg.Input(size=(7,0),  key='numeroopcoes')]   
+            [sg.Text('Largura:', size=(7)), sg.Input(size=(7, 0), pad=((0, 200), (0, 0)), key='numeroperguntas'),
+             sg.Text('Altura:', size=(7, 0), ), sg.Input(size=(7, 0), key='numeroopcoes')]
         ]
 
         layoutEspacamentoPerguntas = [
-            [sg.Text('Perguntas:',size=(7)), sg.Input(size=(7,0), pad=((0, 200), (0, 0)), key='numeroperguntas'), 
-            sg.Text('Respostas:',size=(7,0),), sg.Input(size=(7,0),  key='numeroopcoes')] 
+            [sg.Text('Perguntas:', size=(7)), sg.Input(size=(7, 0), pad=((0, 200), (0, 0)), key='numeroperguntas'),
+             sg.Text('Respostas:', size=(7, 0), ), sg.Input(size=(7, 0), key='numeroopcoes')]
         ]
 
         layoutTipoMarcadores = [
-            [sg.Text('Folha de respostas:',size=(15)), sg.Input(size=(7,0), key='numeroperguntas'), 
-            sg.Text('Perguntas:',size=(7,0),), sg.Input(size=(7,0),  key='numeroopcoes'),
-            sg.Text('Respostas:',size=(7,0),), sg.Input(size=(7,0),  key='numeroopcoes')] 
+            [sg.Text('Folha de respostas:', size=(15)), sg.Input(size=(7, 0), key='numeroperguntas'),
+             sg.Text('Perguntas:', size=(7, 0), ), sg.Input(size=(7, 0), key='numeroopcoes'),
+             sg.Text('Respostas:', size=(7, 0), ), sg.Input(size=(7, 0), key='numeroopcoes')]
         ]
 
         layoutBotoes = [
@@ -38,7 +40,7 @@ class TelaLeitorCartao:
             [sg.Frame('Número', layoutNumeroPerguntas, border_width=2, size=(500, 50))],
             [sg.Frame('Espaçamento Marcador', layoutEspacamentoMarcador, border_width=2, size=(500, 50))],
             [sg.Frame('Espaçamento Respostas', layoutEspacamentoPerguntas, border_width=2, size=(500, 50))],
-            [sg.Frame('Marcadores Respostas', layoutTipoMarcadores, border_width=2, size=(500, 50))],            
+            [sg.Frame('Marcadores Respostas', layoutTipoMarcadores, border_width=2, size=(500, 50))],
             [sg.Frame('', layoutBotoes, border_width=2, size=(500, 50))],
             # [sg.Output(size=(30,20))]
         ]
@@ -47,7 +49,7 @@ class TelaLeitorCartao:
         self.janela = sg.Window("Dados do Usuário").layout(layout)
         # Extrair os dados da tela
         self.button, self.values = self.janela.Read()
-    
+
     def atualizar_label(self, texto):
         self.janela['arquivo'].update(texto)
 
@@ -62,5 +64,10 @@ class TelaLeitorCartao:
                 arquivo_selecionado = sg.popup_get_file("Selecione um arquivo")
                 if arquivo_selecionado:
                     self.controlador.abrir_explorador_de_arquivos(arquivo_selecionado)
+            elif evento == 'Preview':
+                # Abra a classe Formulario
+                formulario = Formulario()
+                formulario.preencher_formulario("Nome do usuário", "Email do usuário")
+                formulario.gerar_pdf("arquivo.pdf")
 
 
